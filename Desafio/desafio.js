df535573297b121cf997f86c81a61693eaf48feb
madulima09 <marydulima23@gmail.com>
2024-07-14 18:46:06 -0300

    Desafio Surpresa 14/07/2024

---
+++ Desafio/desafio.js
@@ -0,0 +1,47 @@
+// Poochyena Masculino
+let pokemon1 = "Poochyena "
+let levelPokemon1 = 2
+let sexoPokemon1 = "M"
+let vidaPokemon1 = "13/13"
+
+// Zigzagoon Feminino
+let pokemon2 = "Zigzagoon "
+let levelPokemon2 = 2
+let sexoPokemon2 = "F"
+let vidaPokemon2 = "13/13"
+
+// Dragonite Masculino
+let pokemon3 = "Dragonite "
+let levelPokemon3 = 5
+let sexoPokemon3 = "M"
+let vidaPokemon3 = "25/25"
+
+// Dragonite Feminino
+let pokemon4 = "Dragonite "
+let levelPokemon4 = 5
+let sexoPokemon4 = "F"
+let vidaPokemon4 = "24/24"
+
+// Dragonite Feminino 2
+let pokemon5 = "Dragonite "
+let levelPokemon5 = 5
+let sexoPokemon5 = "F"
+let vidaPokemon5 = "24/24"
+
+// Poochyena Feminino
+let pokemon6 = "Poochyena "
+let levelPokemon6 = 3
+let sexoPokemon6 = "F"
+let vidaPokemon6 = "15/15"
+
+// Wurmple Masculino
+let pokemon7 = "Wurmple "
+let levelPokemon7 = 2
+let sexoPokemon7 = "M"
+let vidaPokemon7 = "7/14"
+
+
+let mensagemDeSaida = "Cadastrado com Sucesso "
+
+console.log(pokemon7 + mensagemDeSaida)
+
